e0d1efe7440406fbe2cd77f21ebaf95731ec3c26
Takayuki Nakata <f.seasons017@gmail.com>
2021-01-27 00:44:08 +0900

    Add user event list

---
+++ examples/furikaeri.rs
@@ -0,0 +1,82 @@
+use ghrs::{Client, EventType, Payload};
+
+use ureq::Error;
+
+fn main() {
+    match run() {
+        Ok(_) => {}
+        Err(Error::Status(_code, status)) => {
+            println!("Request failed: {:?}", status);
+        }
+        Err(Error::Transport(transport)) => {
+            println!("{}", transport);
+        }
+    }
+}
+
+fn run() -> Result<(), ureq::Error> {
+    let events = Client::activity().events().list_user_events("giraffate")?;
+
+    let mut issues_events = Vec::new();
+    let mut pull_request_events = Vec::new();
+    let mut pull_request_review_comment_events = Vec::new();
+    let mut issue_comment_events = Vec::new();
+    let mut commit_comment_events = Vec::new();
+    for event in events.into_iter() {
+        match event.r#type {
+            EventType::IssuesEvent => issues_events.push(event),
+            EventType::PullRequestEvent => pull_request_events.push(event),
+            EventType::PullRequestReviewCommentEvent => {
+                pull_request_review_comment_events.push(event)
+            }
+            EventType::IssueCommentEvent => issue_comment_events.push(event),
+            EventType::CommitCommentEvent => commit_comment_events.push(event),
+            _ => {}
+        }
+    }
+
+    println!("## Issues Event");
+    for event in issues_events {
+        if let Payload::IssuesEventPayload(payload) = event.payload.unwrap() {
+            println!("- [{}]({})", payload.issue.title, payload.issue.html_url);
+        }
+    }
+    println!("");
+    println!("## Pull Request Event");
+    for event in pull_request_events {
+        if let Payload::PullRequestEventPayload(payload) = event.payload.unwrap() {
+            println!(
+                "- [{}][{}]",
+                payload.pull_request.title, payload.pull_request.html_url
+            );
+        }
+    }
+    println!("");
+    println!("## Pull Request Review Comment Event");
+    for event in pull_request_review_comment_events {
+        if let Payload::PullRequestReviewCommentEventPayload(payload) = event.payload.unwrap() {
+            println!(
+                "- [{}][{}]",
+                payload.pull_request.title, payload.comment.html_url
+            );
+        }
+    }
+    println!("");
+    println!("## Issue Comment Event");
+    for event in issue_comment_events {
+        if let Payload::IssueCommentEventPayload(payload) = event.payload.unwrap() {
+            println!("- [{}][{}]", payload.issue.title, payload.comment.html_url);
+        }
+    }
+    println!("");
+    println!("## Commit Comment Event");
+    for event in commit_comment_events {
+        if let Payload::CommitCommentEventPayload(payload) = event.payload.unwrap() {
+            println!(
+                "- [{}][{}]",
+                payload.comment.html_url, payload.comment.html_url
+            );
+        }
+    }
+    Ok(())
+}
