e6ef0e920b84fd2ce8979a204837a8e26ccdd4a3
Takayuki Nakata <f.seasons017@gmail.com>
2021-01-28 23:21:04 +0900

    Split files

---
+++ src/events.rs
@@ -0,0 +1,76 @@
+use crate::model::Event;
+
+/// A client for the Event API.
+///
+/// See <https://docs.github.com/en/rest/reference/activity#events>.
+pub struct EventsHandler;
+
+impl EventsHandler {
+    pub fn new() -> EventsHandler {
+        EventsHandler {}
+    }
+
+    pub fn list_user_events(&self, user: impl Into<String>) -> ListUserEventsBuilder {
+        ListUserEventsBuilder::new(user)
+    }
+}
+
+/// A builder for listing user events
+pub struct ListUserEventsBuilder {
+    user: String,
+    accept: Option<String>,
+    per_page: Option<u8>,
+    page: Option<u8>,
+}
+
+impl ListUserEventsBuilder {
+    fn new(user: impl Into<String>) -> ListUserEventsBuilder {
+        ListUserEventsBuilder {
+            user: user.into(),
+            accept: None,
+            per_page: None,
+            page: None,
+        }
+    }
+
+    /// List events for the authenticated user.
+    ///
+    /// See <https://docs.github.com/en/rest/reference/activity#list-events-for-the-authenticated-user>.
+    /// ```no_run
+    /// let events = ghrs::Client::events().list_user_events("user").per_page(100).send();
+    /// ```
+    pub fn send(&self) -> Result<Vec<Event>, ureq::Error> {
+        let mut request = ureq::get(&format!(
+            "https://api.github.com/users/{}/events",
+            self.user
+        ));
+
+        if let Some(accept) = self.accept.clone() {
+            request = request.set("Accept", &accept);
+        }
+        if let Some(per_page) = self.per_page {
+            request = request.query("per_page", &per_page.to_string());
+        }
+        if let Some(page) = self.page {
+            request = request.query("page", &page.to_string());
+        }
+
+        let user_events: Vec<Event> = request.call()?.into_json()?;
+        Ok(user_events)
+    }
+
+    pub fn accept(mut self, accept: impl Into<String>) -> Self {
+        self.accept = Some(accept.into());
+        self
+    }
+
+    pub fn per_page(mut self, per_page: impl Into<u8>) -> Self {
+        self.per_page = Some(per_page.into());
+        self
+    }
+
+    pub fn page(mut self, page: impl Into<u8>) -> Self {
+        self.page = Some(page.into());
+        self
+    }
+}
---
+++ src/pulls.rs
@@ -0,0 +1,140 @@
+use crate::model::PullRequest;
+
+/// A client for the Pull Request API.
+///
+/// See <https://docs.github.com/en/rest/reference/pulls>.
+pub struct PullsHandler {
+    owner: String,
+    repo: String,
+}
+
+impl PullsHandler {
+    pub fn new(owner: impl Into<String>, repo: impl Into<String>) -> PullsHandler {
+        PullsHandler {
+            owner: owner.into(),
+            repo: repo.into(),
+        }
+    }
+
+    /// List pull requests.
+    ///
+    /// See <https://docs.github.com/en/rest/reference/pulls#list-pull-requests>.
+    /// ```no_run
+    /// let pull_requests = ghrs::Client::pulls("owner", "repo").list().per_page(100).send();
+    /// ```
+    pub fn list(&self) -> ListPullRequestsBuilder {
+        ListPullRequestsBuilder::new(&self)
+    }
+
+    /// Get a pull request.
+    ///
+    /// See <https://docs.github.com/en/rest/reference/pulls#get-a-pull-request>.
+    /// ```no_run
+    /// let pull_request = ghrs::Client::pulls("owner", "repo").get(1234).send();
+    /// ```
+    pub fn get(&self, pull_number: u64) -> GetPullRequestBuilder {
+        GetPullRequestBuilder::new(&self, pull_number)
+    }
+}
+
+/// A builder for listing pull requests.
+pub struct ListPullRequestsBuilder<'a> {
+    handler: &'a PullsHandler,
+    accept: Option<String>,
+    per_page: Option<u8>,
+    page: Option<u8>,
+}
+
+impl<'a> ListPullRequestsBuilder<'a> {
+    pub fn new(handler: &'a PullsHandler) -> Self {
+        ListPullRequestsBuilder {
+            handler,
+            accept: None,
+            per_page: None,
+            page: None,
+        }
+    }
+
+    /// List pull requests.
+    ///
+    /// See <https://docs.github.com/en/rest/reference/pulls#list-pull-requests>.
+    /// ```no_run
+    /// let pull_requests = ghrs::Client::pulls("owner", "repo").list().per_page(100).send();
+    /// ```
+    pub fn send(&self) -> Result<Vec<PullRequest>, ureq::Error> {
+        let mut request = ureq::get(&format!(
+            "https://api.github.com/repos/{}/{}/pulls",
+            self.handler.owner, self.handler.repo
+        ));
+
+        if let Some(accept) = self.accept.clone() {
+            request = request.set("Accept", &accept);
+        }
+        if let Some(per_page) = self.per_page {
+            request = request.query("per_page", &per_page.to_string());
+        }
+        if let Some(page) = self.page {
+            request = request.query("page", &page.to_string());
+        }
+
+        let pull_requests: Vec<PullRequest> = request.call()?.into_json()?;
+        Ok(pull_requests)
+    }
+
+    pub fn accept(mut self, accept: impl Into<String>) -> Self {
+        self.accept = Some(accept.into());
+        self
+    }
+
+    pub fn per_page(mut self, per_page: impl Into<u8>) -> Self {
+        self.per_page = Some(per_page.into());
+        self
+    }
+
+    pub fn page(mut self, page: impl Into<u8>) -> Self {
+        self.page = Some(page.into());
+        self
+    }
+}
+
+/// A builder for getting a pull request.
+pub struct GetPullRequestBuilder<'a> {
+    handler: &'a PullsHandler,
+    pull_number: u64,
+    accept: Option<String>,
+}
+
+impl<'a> GetPullRequestBuilder<'a> {
+    fn new(handler: &'a PullsHandler, pull_number: u64) -> Self {
+        GetPullRequestBuilder {
+            handler,
+            pull_number,
+            accept: None,
+        }
+    }
+
+    /// Get a pull request.
+    ///
+    /// See <https://docs.github.com/en/rest/reference/pulls#get-a-pull-request>.
+    /// ```no_run
+    /// let pull_request = ghrs::Client::pulls("owner", "repo").get(1234).send();
+    /// ```
+    pub fn send(&self) -> Result<PullRequest, ureq::Error> {
+        let mut request = ureq::get(&format!(
+            "https://api.github.com/repos/{}/{}/pulls/{}",
+            self.handler.owner, self.handler.repo, self.pull_number
+        ));
+
+        if let Some(accept) = self.accept.clone() {
+            request = request.set("Accept", &accept);
+        }
+
+        let pull_request: PullRequest = request.call()?.into_json()?;
+        Ok(pull_request)
+    }
+
+    pub fn accept(mut self, accept: impl Into<String>) -> Self {
+        self.accept = Some(accept.into());
+        self
+    }
+}
